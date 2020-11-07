211c467a465a451f1779da251e4bdcdf406dff83
unknown <diyantonchev@gmail.com>
2020-11-07 11:40:51 +0200

    Updated the gatsby version

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
+
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
